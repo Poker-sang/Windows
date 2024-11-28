377624559698544f12b19464e5228c1416e9f899
Poker <poker_sang@outlook.com>
2024-11-28 17:10:41 +0800

    Update StaggeredLayout.cs

--- components/Primitives/src/StaggeredLayout/StaggeredLayout.cs
+++ components/Primitives/src/StaggeredLayout/StaggeredLayout.cs
@@ -163,7 +163,7 @@ public partial class StaggeredLayout : VirtualizingLayout
         if (ItemsStretch is StaggeredLayoutItemsStretch.None)
         {
             columnWidth = double.IsNaN(DesiredColumnWidth) ? availableWidth : Math.Min(DesiredColumnWidth, availableWidth);
-            numColumns = Math.Max(1, (int)Math.Floor(availableWidth / columnWidth));
+            numColumns = Math.Max(1, (int)Math.Floor(availableWidth / (DesiredColumnWidth + ColumnSpacing)));
         }
         else
         {
